5177525db0599442fedd1749565f5fee3c6d3e7e
Francis <kikobilas123@gmail.com>
2025-05-17 16:43:36 +0800

    Updated: Setup the react redux store and slices

---
+++ client/src/lib/auth/authSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type UserState = {
+  user: {
+    _id: string;
+    name: string;
+    email: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+  } | null;
+  isSigningup: boolean;
+  isSigniningin: boolean;
+  error: string | null;
+  message: string | null;
+};
+
+const initialState: UserState = {
+  user: null,
+  isSigningup: false,
+  isSigniningin: false,
+  error: null,
+  message: null,
+};
+
+export const authSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+});
