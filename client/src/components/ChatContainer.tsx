31ff10e6c73edfa332c1dad67f6dcf3fca166a20
Francis <kikobilas123@gmail.com>
2025-05-25 00:29:52 +0800

    feat: add the header UI for chat box/container

--- client/src/components/ChatContainer.tsx
+++ client/src/components/ChatContainer.tsx
@@ -1,16 +1,27 @@
 import { useAppSelector } from "../lib/Hook";
+import Full from "../assets/addfullscreen.svg";
+import Close from "../assets/close.svg";
 
 const ChatContainer = () => {
   const { currUser } = useAppSelector((state) => state.message);
 
   return (
     <aside className="chat-box">
-      <header className="bg-[#f2c078] p-1">
+      <header className="bg-[#f2c078] p-1 flex justify-between items-center">
         <img
           src={currUser?.image}
           alt=""
           className="aspect-square w-9 rounded-full"
         />
+
+        <div className="flex items-center gap-2">
+          <img src={Full} alt="" className="w-6 aspect-square cursor-pointer" />
+          <img
+            src={Close}
+            alt=""
+            className="w-6 aspect-square cursor-pointer"
+          />
+        </div>
       </header>
     </aside>
   );
